76ff989871373fc81115992a1f20caae6634acd2
Ben Swanson <benswanson127@gmail.com>
2017-07-10 20:54:34 -0600

    Update PrimeFactorization.java

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class PrimeFactorization {
 
 	public static void main(String[] args) {
-		//600851475143
+		//ex. 600851475143
 		long answer = largestFactor();//Enter the number you want to find largest prime factor for here
 		System.out.println(answer);
 	}
